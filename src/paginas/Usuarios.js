e7193583f21b5d3f66d81ff3ea26b6941e0341ed
brigitte <mbpadilla4@misena.edu.co>
2023-11-10 15:59:22 -0500

    Actualizando la pagina de usuarios

--- src/paginas/Usuarios.js
+++ src/paginas/Usuarios.js
@@ -5,6 +5,7 @@ import MenuSecundario from "../componentes/MenuSecundario";
 import PieDePagina from "../componentes/PieDePagina";
 import Texto from "../componentes/Texto";
 import TituloPrincipal from "../componentes/TituloPrincipal";
+import '../estilos/Usuarios.css';
 
 function Usuarios(){
     return(
@@ -14,12 +15,24 @@ function Usuarios(){
             <TituloPrincipal
             titulo='GESTIÓN DE USUARIOS' />
             <Texto
-            descripcion='Este módulo sirve para crear la cuenta de los usuarios asignados a los diferentes proyectos de la compañía' />
+            descripcion='Este módulo sirve para crear la' />
+            <Texto
+            descripcion='cuenta de los usuarios asignados a' />
+            <Texto
+            descripcion='los diferentes proyectos de la' />
+            <Texto
+            descripcion='compañía.' />
+            <div className="caja-grande">
+            <div className="caja-izq">
             <BotonPequeño
             texto='Crear cuenta'
             link={'/cuentauno'} />
+            </div>
+            <div className="caja-der">
             <BotonPequeño
             texto='Iniciar sesión' />
+            </div>
+            </div>
             <PieDePagina />
         </div>
     );
