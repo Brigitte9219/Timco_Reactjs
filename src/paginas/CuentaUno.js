5c95fa51808fedfa3c00fff553ea8c2ffe2fbb0a
brigitte <mbpadilla4@misena.edu.co>
2023-11-10 16:32:05 -0500

    Actualizando la pagina Cuentauno

--- src/paginas/CuentaUno.js
+++ src/paginas/CuentaUno.js
@@ -5,6 +5,7 @@ import FormInput from "../componentes/FormInput";
 import MenuSecundario from "../componentes/MenuSecundario";
 import PieDePagina from "../componentes/PieDePagina";
 import TituloPrincipal from "../componentes/TituloPrincipal";
+import '../estilos/CuentaUno.css';
 
 function CuentaUno(){
     return(
@@ -13,24 +14,30 @@ function CuentaUno(){
             <MenuSecundario />
             <TituloPrincipal
             titulo='Crear cuenta' />
+            <div className="contenedor-formulario">
+            <div className="formulario-izq">
             <FormInput
             type='text'
             placeholder='Nombre' />
             <FormInput
-            type='text'
-            placeholder='Apellido' />
-            <FormInput
             type='email'
             placeholder='usuario@timco.com' />
+            <FormInput
+            type='text'
+            placeholder='Proyecto' />
+            </div>
+            <div className="formulario-der">
+            <FormInput
+            type='text'
+            placeholder='Apellido' />
             <FormInput
             type='password'
             placeholder='Contraseña' />
             <FormInput
-            type='text'
-            placeholder='Proyecto' />
-            <FormInput
             type='password'
             placeholder='Confirmar contraseña' />
+            </div>
+            </div>
             <BotonPequeño
             texto='Continuar'
             link={'/cuentados'} />
