cbec92ad3c68a9af8df5fe8931dfe8e0ea3c05c8
brigitte <mbpadilla4@misena.edu.co>
2023-11-10 18:29:59 -0500

    actualizacion de la pagina Contacto

--- src/paginas/Contacto.js
+++ src/paginas/Contacto.js
@@ -8,7 +8,7 @@ import PieDePagina from '../componentes/PieDePagina';
 import Texto from '../componentes/Texto';
 import TituloPrincipal from '../componentes/TituloPrincipal';
 
-function Modulos(){
+function Contacto(){
     return(
         <div>
             <EncabezadoSecundario />
@@ -17,21 +17,29 @@ function Modulos(){
             titulo='CONTACTO' />
             <Texto
             descripcion='Muchas gracias por su interés, si tiene alguna pregunta. ¡escríbanos un mensaje ahora!' />
+            <div className="contenedor-formulario">
+            <div className="formulario-izq">
             <FormInput
             type='text'
             placeholder='Nombres y apellidos' />
+            </div>
+            <div className="formulario-der">
             <FormInput
             type='email'
             placeholder='Correo electrónico' />
-            <FormInput
-            type='text'
-            placeholder='Asunto' />
+            </div>
+            </div>
+            <FormTextarea
+            placeholder='Asunto'
+            columnas='50'
+            filas='1' />
             <FormTextarea
             placeholder='Mensaje'
             columnas='50'
             filas='10' />
             <Texto
             descripcion='A continuación puede adjuntar cualquier archivo que considere para complementar. Tenga en cuenta: La persona encargada se comunicará con usted con los datos diligenciados en este formulario.' />
+            <br />
             <BotonPequeño
             texto='Enviar'
             link={'/contactouno'} />
@@ -40,4 +48,4 @@ function Modulos(){
     );
 }
 
-export default Modulos;
+export default Contacto;
