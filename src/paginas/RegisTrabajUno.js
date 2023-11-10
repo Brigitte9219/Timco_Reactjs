7207490a36e34fe30ae7bec977b563d53c457386
brigitte <mbpadilla4@misena.edu.co>
2023-11-10 17:08:03 -0500

    actualizando la pagina RegisTrabajUno

--- src/paginas/RegisTrabajUno.js
+++ src/paginas/RegisTrabajUno.js
@@ -16,42 +16,48 @@ function RegisTrabajUno(){
             titulo='Registrar' />
             <Texto
             descripcion='A continuación, registre los datos del trabajador que ingresa a la compañía' />
+            <div className="contenedor-formulario">
+            <div className="formulario-izq">
             <FormInput
             type='text'
             placeholder='Nombre completo' />
             <FormInput
-            type='email'
-            placeholder='Correo electrónico' />
-            <FormInput
             type='text'
             placeholder='Tipo de identificación' />
-            <FormInput
-            type='number'
-            placeholder='No de identificación' />
             <FormInput
             type='date'
             placeholder='Fecha de expedición' />
             <FormInput
-            type='text'
-            placeholder='Ciudad de expedición' />
-            <FormInput
             type='date'
             placeholder='Fecha de nacimiento' />
-            <FormInput
-            type='text'
-            placeholder='Lugar de nacimiento' />
             <FormInput
             type='number'
             placeholder='Edad' />
             <FormInput
             type='text'
+            placeholder='Género' />
+            </div>
+            <div className="formulario-der">
+            <FormInput
+            type='email'
+            placeholder='Correo electrónico' />
+            <FormInput
+            type='number'
+            placeholder='No de identificación' />
+            <FormInput
+            type='text'
+            placeholder='Ciudad de expedición' />
+            <FormInput
+            type='text'
+            placeholder='Lugar de nacimiento' />
+            <FormInput
+            type='text'
             placeholder='Estatura' />
             <FormInput
             type='text'
-            placeholder='Género' />
-            <FormInput
-            type='text'
             placeholder='Estado civil' />
+            </div>
+            </div>
             <BotonPequeño
             texto='Continuar'
             link={'/RegisTrabajDos'} />
