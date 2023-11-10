920f25e5a67971543faa0b742aebdfb9ce37fdaa
brigitte <mbpadilla4@misena.edu.co>
2023-11-10 17:15:41 -0500

    actualizando la pagina RegisTrabajDos

--- src/paginas/RegisTrabajDos.js
+++ src/paginas/RegisTrabajDos.js
@@ -16,42 +16,48 @@ function RegisTrabajDos(){
             titulo='Registrar' />
             <Texto
             descripcion='Continúe registrando los datos' />
+            <div className="contenedor-formulario">
+            <div className="formulario-izq">
             <FormInput
             type='text'
             placeholder='RH' />
             <FormInput
             type='text'
-            placeholder='Grupo sanguíneo' />
-            <FormInput
-            type='text'
             placeholder='Localidad' />
-            <FormInput
-            type='text'
-            placeholder='Ciudad de residencia' />
             <FormInput
             type='text'
             placeholder='Barrio' />
             <FormInput
-            type='text'
-            placeholder='Dirección' />
-            <FormInput
             type='number'
             placeholder='Telefono Móvil' />
-            <FormInput
-            type='number'
-            placeholder='Telefono fijo' />
             <FormInput
             type='text'
             placeholder='EPS' />
             <FormInput
             type='text'
+            placeholder='Escolaridad' />
+            </div>
+            <div className="formulario-der">
+            <FormInput
+            type='text'
+            placeholder='Grupo sanguíneo' />
+            <FormInput
+            type='text'
+            placeholder='Ciudad de residencia' />
+            <FormInput
+            type='text'
+            placeholder='Dirección' />
+            <FormInput
+            type='number'
+            placeholder='Telefono fijo' />
+            <FormInput
+            type='text'
             placeholder='Fondo de pensiones' />
             <FormInput
             type='text'
-            placeholder='Escolaridad' />
-            <FormInput
-            type='text'
             placeholder='Profesión' />
+            </div>
+            </div>
             <BotonPequeño
             texto='Continuar'
             link={'/RegisTrabajTres'} />
