571d31fbfadf092ce1bac85d863db71c246f7c72
BrigittePadilla <153017836+BrigittePadilla@users.noreply.github.com>
2024-01-11 15:39:17 -0500

    Se realiza la solicitud POST a Asignar (AsignarUno)

--- src/paginas/AsignarUno.js
+++ src/paginas/AsignarUno.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import BotonPequeño from "../componentes/BotonPequeño";
 import EncabezadoSecundario from "../componentes/EncabezadoSecundario";
 import FormInput from "../componentes/FormInput";
@@ -7,8 +7,69 @@ import MenuSecundario from "../componentes/MenuSecundario";
 import PieDePagina from "../componentes/PieDePagina";
 import Texto from "../componentes/Texto";
 import TituloPrincipal from "../componentes/TituloPrincipal";
+import { useNavigate } from "react-router-dom";
 
-function AsignarUno(){
+const AsignarUno = () => {
+
+    //Permite navegar por la aplicación
+    const navigate =useNavigate();
+
+    //Almacena los datos del formulario
+    const [formData, setFormData] = useState ({
+        fechaAsignacion:"",
+        nombreTrabajador:"",
+        nombreProducto:"",
+        proyecto:"",
+        talla:"",
+        cantidad:"",
+        area:"",
+        observaciones:""
+    });
+
+    //Actualiza el estado al cambiar los campos del formulario
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        console.log(`Campo ${name} cambiado a: ${value}`);
+        setFormData({
+          ...formData,
+          [name]: value,
+        });
+      };
+
+    //Maneja el envío del formulario
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            console.log("Enviando formulario:", formData);
+
+        const response = await fetch("http://localhost:8080/asignacion", {
+            method: "POST",
+            headers: {
+            "Content-Type": "application/json",
+            // Otras cabeceras según sea necesario
+            },
+            body: JSON.stringify(formData),
+        });
+
+        // Manejar la respuesta del servidor (response)
+        if (response.ok) {
+            // Realizar acciones después del éxito
+            console.log("Solicitud POST exitosa");
+
+            // Obtén el ID del trabajador de la respuesta (ajusta según la estructura de tu respuesta)
+            //const { idTrabajador } = await response.json();
+
+            // Redirige a la siguiente página y pasa el ID como parámetro
+            navigate("/asignardos");
+        } else {
+            console.error("Error en la solicitud POST:", response.status, response.statusText);
+        }
+        } catch (error) {
+        console.error("Error al realizar la solicitud POST:", error);
+        }
+    };
+
     return(
 <div>
     <EncabezadoSecundario />
@@ -18,36 +79,68 @@ function AsignarUno(){
     <Texto
     descripcion='Realice la asignación de un producto de acuerdo con la solicitud previa' />
     <div className="contenedor-formulario">
+    <form onSubmit={handleSubmit}>
     <div className="formulario-izq">
     <FormInput
     type='date'
-    placeholder='Fecha de asignación' />
+    name='fechaAsignacion'
+    value={formData.fechaAsignacion}
+    placeholder='Fecha de asignación'
+    onChange={handleChange} />
     <FormInput
     type='text'
-    placeholder='Nombre del producto' />
+    name='nombreTrabajador'
+    value={formData.nombreTrabajador}
+    placeholder='Nombre del trabajador'
+    onChange={handleChange} />
     <FormInput
     type='text'
-    placeholder='Proyecto' />
+    name='nombreProducto'
+    value={formData.nombreProducto}
+    placeholder='Nombre del producto'
+    onChange={handleChange} />
+    <FormInput
+    type='text'
+    name='proyecto'
+    value={formData.proyecto}
+    placeholder='Proyecto'
+    onChange={handleChange} />
     </div>
+
     <div className="formulario-der">
     <FormInput
     type='text'
-    placeholder='Nombre del trabajador' />
+    name='talla'
+    value={formData.talla}
+    placeholder='Talla'
+    onChange={handleChange} />
     <FormInput
     type='number'
-    placeholder='Cantidad' />
+    name='cantidad'
+    value={formData.cantidad}
+    placeholder='Cantidad'
+    onChange={handleChange} />
     <FormInput
     type='text'
-    placeholder='Area' />
-    </div>
+    name='area'
+    value={formData.area}
+    placeholder='Area'
+    onChange={handleChange} />
     </div>
     <FormTextarea
     placeholder='Observaciones'
     columnas='50'
-    filas='10' /><br /><br />
+    filas='10'
+    type='text'
+    name='observaciones'
+    value={formData.observaciones}
+    onChange={handleChange} /><br /><br />
     <BotonPequeño
     texto='Enviar'
+    type='submit'
     link={'/asignardos'} />
+    </form>
+    </div>
     <PieDePagina />
 </div>
     );
