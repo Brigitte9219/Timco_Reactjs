48be9e6d27ef01171a50da1d2e687c44c9fdbd17
BrigittePadilla <153017836+BrigittePadilla@users.noreply.github.com>
2024-01-15 14:40:15 -0500

    Se realiza la solicitud POST para RegisTrabajUno (RegisTrabajDos y RegisTrabajTres ya no estan en servicio)

--- src/paginas/RegisTrabajUno.js
+++ src/paginas/RegisTrabajUno.js
@@ -10,7 +10,8 @@ import { useNavigate } from "react-router-dom"; // Cambia a useNavigate
 
 const RegisTrabajUno = () => {
 
-    const navigate = useNavigate(); // Cambia de useHistory a useNavigate
+    //Esto permite navegar entre la aplicacion
+    const navigate = useNavigate();
 
     //Almacena los datos del formulario
   const [formData, setFormData] = useState({
@@ -25,8 +26,29 @@ const RegisTrabajUno = () => {
     ciudadExpedicion:"",
     lugarNacimiento:"",
     estatura:"",
-    estadoCivil:""
+    estadoCivil:"",
     // ...otros campos del formulario
+    rh:"",
+    grupoSanguineo:"",
+    localidad:"",
+    ciudadResidencia:"",
+    barrio:"",
+    direccion:"",
+    telefonoMovil:"",
+    telefonoFijo:"",
+    eps:"",
+    afp:"",
+    escolaridad:"",
+    profesion:"",
+    // ...otros campos del formulario
+    tallaCamisa:"",
+    tallaPantalon:"",
+    tallaChaqueta:"",
+    tallaCalzado:"",
+    nombreEmergencia:"",
+    parentescoEmergencia:"",
+    movilEmergencia:"",
+    fijoEmergencia:""
   });
 
   //Actualiza el estado al cambiar los campos del formulario
@@ -39,9 +61,46 @@ const RegisTrabajUno = () => {
     });
   };
 
-  //Maneja el envío del formulario
+    //Maneja el envío del formulario
   const handleSubmit = async (e) => {
     e.preventDefault();
+
+    if(
+      !formData.nombre ||
+      !formData.tipoId ||
+      !formData.fechaExpedicion ||
+      !formData.fechaNacimiento ||
+      !formData.edad ||
+      !formData.genero ||
+      !formData.correo ||
+      !formData.numeroId ||
+      !formData.ciudadExpedicion ||
+      !formData.lugarNacimiento ||
+      !formData.estatura ||
+      !formData.estadoCivil ||
+      !formData.rh ||
+      !formData.grupoSanguineo ||
+      !formData.localidad ||
+      !formData.ciudadResidencia ||
+      !formData.barrio ||
+      !formData.direccion ||
+      !formData.telefonoMovil ||
+      !formData.telefonoFijo ||
+      !formData.eps ||
+      !formData.afp ||
+      !formData.escolaridad ||
+      !formData.profesion ||
+      !formData.tallaCamisa ||
+      !formData.tallaPantalon ||
+      !formData.tallaChaqueta ||
+      !formData.tallaCalzado ||
+      !formData.nombreEmergencia ||
+      !formData.parentescoEmergencia ||
+      !formData.movilEmergencia ||
+      !formData.fijoEmergencia
+    ){
+      alert('Por favor, complete todos los campos obligatorios');
+    }
 
     try {
         console.log("Enviando formulario:", formData);
@@ -60,11 +119,14 @@ const RegisTrabajUno = () => {
         // Realizar acciones después del éxito
         console.log("Solicitud POST exitosa");
 
+        //Mostrar una alerta al usuario
+        alert('El proceso de registro ha terminado de manera exitosa.');
+
         // Obtén el ID del trabajador de la respuesta (ajusta según la estructura de tu respuesta)
-        const { idTrabajador } = await response.json();
+        //const { idTrabajador } = await response.json();
 
         // Redirige a la siguiente página y pasa el ID como parámetro
-        navigate(`/registrabajdos/${idTrabajador}`);
+        navigate("/registrabajcuatro");
       } else {
         console.error("Error en la solicitud POST");
       }
@@ -117,7 +179,7 @@ const RegisTrabajUno = () => {
               name='genero'
               value={formData.genero}
               placeholder='Género'
-              onChange={handleChange} />
+              onChange={handleChange} />              
           </div>
 
           <div className="formulario-der">
@@ -157,11 +219,135 @@ const RegisTrabajUno = () => {
               value={formData.estadoCivil}
               placeholder='Estado civil'
               onChange={handleChange} />
+              <FormInput
+            type='text'
+            name='rh'
+            value={formData.rh}
+            placeholder='RH'
+            onChange={handleChange} />
+            <FormInput
+            type='text'
+            name='localidad'
+            value={formData.localidad}
+            placeholder='Localidad'
+            onChange={handleChange} />
+            <FormInput
+            type='text'
+            name='barrio'
+            value={formData.barrio}
+            placeholder='Barrio'
+            onChange={handleChange} />
+            <FormInput
+            type='number'
+            name='telefonoMovil'
+            value={formData.telefonoMovil}
+            placeholder='Telefono Móvil'
+            onChange={handleChange} />
+            <FormInput
+            type='text'
+            name='eps'
+            value={formData.eps}
+            placeholder='EPS'
+            onChange={handleChange} />
+            <FormInput
+            type='text'
+            name='escolaridad'
+            value={formData.escolaridad}
+            placeholder='Escolaridad'
+            onChange={handleChange} />
+            <FormInput
+            type='text'
+            name='grupoSanguineo'
+            value={formData.grupoSanguineo}
+            placeholder='Grupo sanguíneo'
+            onChange={handleChange} />
+            <FormInput
+            type='text'
+            name='ciudadResidencia'
+            value={formData.ciudadResidencia}
+            placeholder='Ciudad de residencia'
+            onChange={handleChange} />
+            <FormInput
+            type='text'
+            name='direccion'
+            value={formData.direccion}
+            placeholder='Dirección'
+            onChange={handleChange} />
+            <FormInput
+            type='number'
+            name='telefonoFijo'
+            value={formData.telefonoFijo}
+            placeholder='Telefono fijo'
+            onChange={handleChange} />
+            <FormInput
+            type='text'
+            name='afp'
+            value={formData.afp}
+            placeholder='Fondo de pensiones'
+            onChange={handleChange} />
+            <FormInput
+            type='text'
+            name='profesion'
+            value={formData.profesion}
+            placeholder='Profesión'
+            onChange={handleChange} />
+            <Texto
+            descripcion='Tallas para dotación' />
+            <FormInput
+            type='text'
+            name='tallaCamisa'
+            value={formData.tallaCamisa}
+            onChange={handleChange}
+            placeholder='Camisa' />
+            <FormInput
+            type='text'
+            name='tallaChaqueta'
+            value={formData.tallaChaqueta}
+            onChange={handleChange}
+            placeholder='Chaqueta' />
+            <FormInput
+            type='text'
+            name='tallaPantalon'
+            value={formData.tallaPantalon}
+            onChange={handleChange}
+            placeholder='Pantalon' />
+            <FormInput
+            type='text'
+            name='tallaCalzado'
+            value={formData.tallaCalzado}
+            onChange={handleChange}
+            placeholder='Calzado' />
+            <Texto
+            descripcion='En caso de emergencia' />
+            <FormInput
+            type='text'
+            name='nombreEmergencia'
+            value={formData.nombreEmergencia}
+            onChange={handleChange}
+            placeholder='Nombre Completo' />
+            <FormInput
+            type='number'
+            name='movilEmergencia'
+            value={formData.movilEmergencia}
+            onChange={handleChange}
+            placeholder='Telefono móvil' />
+            <FormInput
+            type='text'
+            name='parentescoEmergencia'
+            value={formData.parentescoEmergencia}
+            onChange={handleChange}
+            placeholder='Parentesco' />
+            <FormInput
+            type='number'
+            name='fijoEmergencia'
+            value={formData.fijoEmergencia}
+            onChange={handleChange}
+            placeholder='Telefono fijo' />          
           </div>
           <BotonPequeño
             texto='Continuar'
             type='submit'
-            link={'/registrabajdos'} />
+            link={'/registrabajcuatro'} />
         </form>
       </div>
       <PieDePagina />
